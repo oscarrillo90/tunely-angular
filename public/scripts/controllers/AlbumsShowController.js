c938c16b7f39278874284559c084dea733c93c93
oscar carrillo <oscarrillo008@outlook.com>
2017-06-08 09:08:56 -0500

    finished sprint 4

---
+++ public/scripts/controllers/AlbumsShowController.js
@@ -0,0 +1,20 @@
+angular
+  .module('tunely')
+  .controller('AlbumsShowController', AlbumsShowController);
+
+AlbumsShowController.$inject = ['$http', '$routeParams'];
+
+function AlbumsShowController ($http, $routeParams) {
+  var vm = this;
+  vm.newSong = {};
+
+  $http({
+    method: 'GET',
+    url: '/api/albums/'+$routeParams.id
+  }).then(function successCallback(json) {
+    vm.album = json.data;
+  }, function errorCallback(response) {
+    console.log('There was an error getting the data', response);
+  });
+
+}
